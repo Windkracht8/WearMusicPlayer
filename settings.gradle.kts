2da84915561343293b3b501afcdd3bf2083b9bae
Windkracht8 <dev@windkracht8.com>
2025-08-15 11:15:52 +0200

    fix gradle stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@
  * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-rootProject.name = 'WearMusicPlayer'
+rootProject.name = "WearMusicPlayer"
 
 include(":mobile")
-include("":wear")
+include(":wear")
